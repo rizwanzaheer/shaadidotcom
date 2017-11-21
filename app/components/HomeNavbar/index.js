71515c5bc176022958c04c6f3adcf3615c517ea8
RIZWAN\Rizwan <rizwanzaheer003@gmail.com>
2017-11-21 19:21:17 +0500

    change site logo url

--- app/components/HomeNavbar/index.js
+++ app/components/HomeNavbar/index.js
@@ -4,36 +4,33 @@
 *
 */
 
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../images/home-logo.png';
 // import styled from 'styled-components';
 
-// import { FormattedMessage } from 'react-intl';
-// import messages from './messages';
-import "./HomeNavbar.scss";
+import './HomeNavbar.scss';
 
 class HomeNavbar extends React.Component {
   // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <div className="HomeNavbar-container">
-        {/* <div className="home-navbar-wrapper"> */}
-          <Link to="/" className="home_logo">
-          </Link>
-          <Link to="#" className="right help-btn">
-            Help
-            <i className="fa fa-angle-down" aria-hidden="true" />
-          </Link>
-          <Link to="#" className="right customer-care-avatar">
-            <span />
-          </Link>
-          <Link to="/test" className="right login-btn">
-            Login
-            <i className="fa fa-angle-down" aria-hidden="true" />
-            <span />
-          </Link>
-        {/* </div> */}
-        {/* <FormattedMessage {...messages.header} /> */}
+        <Link to="/">
+          <img src={Logo} alt="" />
+        </Link>
+        <Link to="#" className="right help-btn">
+          Help
+          <i className="fa fa-angle-down" aria-hidden="true" />
+        </Link>
+        <Link to="#" className="right customer-care-avatar">
+          <span />
+        </Link>
+        <Link to="/test" className="right login-btn">
+          Login
+          <i className="fa fa-angle-down" aria-hidden="true" />
+          <span />
+        </Link>
       </div>
     );
   }
