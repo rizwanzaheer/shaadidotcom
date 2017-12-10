c42f984b358ddb21240dd5268a04dcec5afac311
Rizwan Zaheer <rizwanzaheer003@gmail.com>
2017-12-10 21:39:10 +0500

    adding input handler & axios

--- app/components/Modal/index.js
+++ app/components/Modal/index.js
@@ -9,7 +9,9 @@ import { FormattedMessage } from 'react-intl';
 import ReactTooltip from 'react-tooltip';
 import { NavLink, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import 'react-responsive-modal/lib/react-responsive-modal.css';
+import EnvChecker from '../../config/envChecker';
 
 // import CrossIcon from 'components/CrossIcon';
 // import ModalWrapper from './ModalStyle';
@@ -17,6 +19,31 @@ import messages from './messages';
 import './ModalStyle.scss';
 class SignInModal extends React.Component {
   // eslint-disable-line react/prefer-stateless-function
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.signinbtnClickHandler = this.signinbtnClickHandler.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value,
+    });
+  }
+  signinbtnClickHandler() {
+    console.log('signinbtnClickHandler!');
+    const { email, password } = this.state;
+    if (!email && !password) {
+      console.log('Please Enter Email & Password!');
+    }
+    // axios.post();
+  }
   render() {
     return (
       <div className="signin-modal">
@@ -35,8 +62,10 @@ class SignInModal extends React.Component {
                     type="email"
                     className="form-control"
                     id="eamil"
+                    name="email"
                     aria-describedby="emailHelp"
                     placeholder="Enter email"
+                    onChange={this.handleInputChange}
                   />
                   <small id="emailHelp" className="form-text text-muted">
                     Well never share your email with anyone else.
@@ -52,7 +81,9 @@ class SignInModal extends React.Component {
                     type="password"
                     className="form-control"
                     id="password"
+                    name="password"
                     placeholder="Password"
+                    onChange={this.handleInputChange}
                   />
                 </div>
               </div>
@@ -93,18 +124,23 @@ class SignInModal extends React.Component {
               <p>not use this if you are</p>
               <p>signing in from a shared computer? </p>
             </ReactTooltip>
-            <Link to="/signin" className="signin-btn">
+            <Link to="#" className="signin-btn">
               <button
                 className="btn text-white waves-effect waves-light modal-action modal-close"
-                type="submit"
-                name="action"
+                type="button"
+                // name="action"
+                onClick={this.signinbtnClickHandler}
               >
                 Signin
               </button>
             </Link>
             <h6>New to Shaadidotcom?</h6>
             <div className="signup-free-btn text-center">
-              <Link to="#" onClick={this.props.signupClickHandler} className="modal-action modal-close">
+              <Link
+                to="#"
+                onClick={this.props.signupClickHandler}
+                className="modal-action modal-close"
+              >
                 Sign Up Free
               </Link>
             </div>
