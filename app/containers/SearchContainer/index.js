6f64d184c9949a89ac7e9b561bd0ac8dd1f04926
Rizwan Zaheer <rizwanzaheer003@gmail.com>
2018-01-07 00:22:01 +0500

    add right sidebar search container

--- app/containers/SearchContainer/index.js
+++ app/containers/SearchContainer/index.js
@@ -16,6 +16,7 @@ import { compose } from 'redux';
 
 import Dropdown from 'components/Dropdown';
 import MoreDetailButtonRight from 'components/MoreDetailButtonRight';
+import RightSidePartnerSearchContainer from 'components/RightSidePartnerSearchContainer';
 
 import injectSaga from 'utils/injectSaga';
 import injectReducer from 'utils/injectReducer';
@@ -59,97 +60,113 @@ export class SearchContainer extends React.Component {
       matrialStatus,
     } = this.state;
     // console.log('click working!');
-   this.props.history.push(`searchusers?gender=${gender}&fromage=${fromAge}&toage=${toAge}&matrialStatus=${matrialStatus}&religion=${religion}&mothertongue=${motherTongue}&community=${community}`);
+    this.props.history.push(
+      `searchusers?gender=${gender}&fromage=${fromAge}&toage=${toAge}&matrialStatus=${matrialStatus}&religion=${religion}&mothertongue=${motherTongue}&community=${community}`
+    );
   }
   render() {
     const { gender, fromAge, toAge, religion, motherTongue } = this.state;
     return (
-      <div className="container advanced-search-container">
+      <div className="container advanced-search-wrapper">
         <Helmet>
           <title>Search Bride/Groom</title>
           <meta name="description" content="Description of SearchContainer" />
         </Helmet>
-        <div className="row single-entity">
-          <div className="col-3">Looking For</div>
-          <div className="col-8">
-            <div className="form-check form-check-inline">
-              <input
-                className="form-check-input"
-                type="radio"
-                name="inlineRadioOptions"
-                id="inlineRadio1"
-                value="option1"
-                onChange={() => this.setState({ gender: 'woman' }, () => console.log(this.state))}
-              />
-              <label className="form-check-label" htmlFor="inlineRadio1">
-                Bride
-              </label>
-            </div>
-            <div className="form-check form-check-inline">
-              <input
-                className="form-check-input"
-                type="radio"
-                name="inlineRadioOptions"
-                id="inlineRadio2"
-                value="option2"
-                onChange={() => this.setState({ gender: 'man' }, () => console.log(this.state))}
-              />
-              <label className="form-check-label" htmlFor="inlineRadio2">
-                Groom
-              </label>
-            </div>
-          </div>
-        </div>
-
-        <div className="row single-entity">
-          <div className="col-3">Age</div>
-          <div className="col-8">
-            <div className="row">
-              <div className="col-5">
-                <Dropdown
-                  options={ageOfDropDown[0].options}
-                  defaultValue={fromAge}
-                  dropDownChangeHandler={this.dropDownChangeHandler}
-                />
-              </div>
-              <div className="col-1">to</div>
-              <div className="col-5">
-                <Dropdown
-                  options={ageOfDropDown[0].options}
-                  defaultValue={toAge}
-                  dropDownChangeHandler={this.dropDownChangeHandler}
-                />
+        <div className="row">
+          <div className="col-8 advanced-search-container">
+            <div className="row single-entity">
+              <div className="col-3">Looking For</div>
+              <div className="col-8">
+                <div className="form-check form-check-inline">
+                  <input
+                    className="form-check-input"
+                    type="radio"
+                    name="inlineRadioOptions"
+                    id="inlineRadio1"
+                    value="option1"
+                    onChange={() =>
+                      this.setState({ gender: 'woman' }, () =>
+                        console.log(this.state)
+                      )
+                    }
+                  />
+                  <label className="form-check-label" htmlFor="inlineRadio1">
+                    Bride
+                  </label>
+                </div>
+                <div className="form-check form-check-inline">
+                  <input
+                    className="form-check-input"
+                    type="radio"
+                    name="inlineRadioOptions"
+                    id="inlineRadio2"
+                    value="option2"
+                    onChange={() =>
+                      this.setState({ gender: 'man' }, () =>
+                        console.log(this.state)
+                      )
+                    }
+                  />
+                  <label className="form-check-label" htmlFor="inlineRadio2">
+                    Groom
+                  </label>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
 
-        {FIELDGENERATER.map(({ data, name }) => (
-          <div className="row single-entity" key={name}>
-            <div className="col-3">{name}</div>
-            <div className="col-8">
-              <Dropdown
-                options={data}
-                defaultValue=""
-                dropDownChangeHandler={this.dropDownChangeHandler}
-              />
+            <div className="row single-entity">
+              <div className="col-3">Age</div>
+              <div className="col-8">
+                <div className="row">
+                  <div className="col-5">
+                    <Dropdown
+                      options={ageOfDropDown[0].options}
+                      defaultValue={fromAge}
+                      dropDownChangeHandler={this.dropDownChangeHandler}
+                    />
+                  </div>
+                  <div className="col-1">to</div>
+                  <div className="col-5">
+                    <Dropdown
+                      options={ageOfDropDown[0].options}
+                      defaultValue={toAge}
+                      dropDownChangeHandler={this.dropDownChangeHandler}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {FIELDGENERATER.map(({ data, name }) => (
+              <div className="row single-entity" key={name}>
+                <div className="col-3">{name}</div>
+                <div className="col-8">
+                  <Dropdown
+                    options={data}
+                    defaultValue=""
+                    dropDownChangeHandler={this.dropDownChangeHandler}
+                  />
+                </div>
+              </div>
+            ))}
+            <div className="row text-center">
+              <div className="col-12">
+                <span
+                  className="advanced-search-btn"
+                  disable
+                  onClick={() => {
+                    this.linkCreation();
+                    console.log('this state: ', this.state);
+                  }}
+                >
+                  <MoreDetailButtonRight label="Search" url="#" />
+                </span>
+                &nbsp;&nbsp;&nbsp;{' '}
+                <MoreDetailButtonRight label="Reset" url="#" />
+              </div>
             </div>
           </div>
-        ))}
-        <div className="row text-center">
-          <div className="col-12">
-            <span
-              className="advanced-search-btn"
-              disable
-              onClick={() => {
-                this.linkCreation();
-                console.log('this state: ', this.state);
-              }}
-            >
-              <MoreDetailButtonRight label="Search" url='#' />
-            </span>
-            &nbsp;&nbsp;&nbsp; <MoreDetailButtonRight label="Reset" url='#' />
-          </div>
+          <RightSidePartnerSearchContainer />
         </div>
       </div>
     );
@@ -175,4 +192,6 @@ const withConnect = connect(mapStateToProps, mapDispatchToProps);
 const withReducer = injectReducer({ key: 'searchContainer', reducer });
 const withSaga = injectSaga({ key: 'searchContainer', saga });
 
-export default withRouter(compose(withReducer, withSaga, withConnect)(SearchContainer));
+export default withRouter(
+  compose(withReducer, withSaga, withConnect)(SearchContainer)
+);
